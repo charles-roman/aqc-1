4446911000447033975cd99d7beb8ea1721f3a8f
croman <charlesroman@icloud.com>
2025-10-11 18:09:29 -0700

    Update and clean-up time module
    - deletes get_timestamp function
    - deletes periperal clock reference macro (APB1_CLK_FREQ_MHZ)
    - deletes external timer handle variables
    - deletes custom timer struct handle
    - swaps out included headers for only necessary ones
    - adds static globals to cache timer clock reference frequencies
    - adds API to retrieve timer clock reference frequencies
    - cleans up existing functions and comments

--- Core/Src/common/time.c
+++ Core/Src/common/time.c
@@ -5,79 +5,140 @@
  *      Author: charlieroman
  */
 
-#include "main.h"
-#include "maths.h"
 #include "time.h"
 
+/* Timer Clock Reference Freqs */
+static uint32_t TIM2_CLK_REF_FREQ_HZ;
+static uint32_t TIM3_CLK_REF_FREQ_HZ;
+static uint32_t TIM4_CLK_REF_FREQ_HZ;
+static uint32_t TIM6_CLK_REF_FREQ_HZ;
+static uint32_t TIM8_CLK_REF_FREQ_HZ;
+
 /*
- * @brief  delay
+ * @brief  get peripheral bus 1 timer clock freq
  *
- * @param  ms       delay in ms
- * @retval None
+ * @param  None
+ * @retval freq (hz)
  */
-void delay(uint32_t ms)
-{
-  HAL_Delay(ms);
+static uint32_t Get_APB1_TIMCLKFreq(void) {
+	uint32_t APB1_PCLK_FREQ_HZ, APB1_TIMCLK_FREQ_HZ;
+
+	/* Get APB1 Clock Freq */
+    APB1_PCLK_FREQ_HZ = HAL_RCC_GetPCLK1Freq();
+
+    /* Get APB1 Timer Clock Freq */
+    APB1_TIMCLK_FREQ_HZ = (RCC->CFGR & RCC_CFGR_PPRE1_Msk) != RCC_CFGR_PPRE1_DIV1 ?
+    					  APB1_PCLK_FREQ_HZ * 2 : APB1_PCLK_FREQ_HZ; // doubled if PSC > 1
+
+    return APB1_TIMCLK_FREQ_HZ;
 }
 
 /*
- * @brief  timer start
+ * @brief  get peripheral bus 2 timer clock freq
  *
- * @param  htim		pointer to HAL timer struct
+ * @param  None
+ * @retval freq (hz)
+ */
+static uint32_t Get_APB2_TIMCLKFreq(void) {
+	uint32_t APB2_PCLK_FREQ_HZ, APB2_TIMCLK_FREQ_HZ;
+
+	/* Get APB1 Clock Freq */
+    APB2_PCLK_FREQ_HZ = HAL_RCC_GetPCLK2Freq();
+
+    /* Get APB1 Timer Clock Freq */
+    APB2_TIMCLK_FREQ_HZ = (RCC->CFGR & RCC_CFGR_PPRE2_Msk) != RCC_CFGR_PPRE2_DIV1 ?
+    					  APB2_PCLK_FREQ_HZ * 2 : APB2_PCLK_FREQ_HZ; // doubled if PSC > 1
+
+    return APB2_TIMCLK_FREQ_HZ;
+}
+
+/*
+ * @brief  caches desired timer clock reference frequency for future use
+ *
+ * @param  htim       pointer to HAL timer handle
  * @retval None
  */
-void start_timer(TIM_HandleTypeDef *htim)
-{
+void Cache_TIMCLKRefFreq(TIM_HandleTypeDef *htim) {
+	if (htim == NULL)
+		return; // Setup_Error_Handler();
+
+	if (htim->Instance == TIM2) {
+		TIM2_CLK_REF_FREQ_HZ = Get_APB1_TIMCLKFreq() / (htim->Init.Prescaler + 1);
+
+	} else if (htim->Instance == TIM3) {
+		TIM3_CLK_REF_FREQ_HZ = Get_APB1_TIMCLKFreq() / (htim->Init.Prescaler + 1);
+
+	} else if (htim->Instance == TIM4) {
+		TIM4_CLK_REF_FREQ_HZ = Get_APB1_TIMCLKFreq() / (htim->Init.Prescaler + 1);
+
+	} else if (htim->Instance == TIM6) {
+		TIM6_CLK_REF_FREQ_HZ = Get_APB1_TIMCLKFreq() / (htim->Init.Prescaler + 1);
+
+	} else if (htim->Instance == TIM8) {
+		TIM8_CLK_REF_FREQ_HZ = Get_APB2_TIMCLKFreq() / (htim->Init.Prescaler + 1);
+
+	} else {
+		// Setup_Error_Handler();
+	}
+
+}
+
+/*
+ * @brief  retrieves the timer 2 clock reference freq
+ *
+ * @param  None
+ * @retval freq (hz)
+ */
+uint32_t Get_TIM2CLKRefFreqHz(void) {
+	return TIM2_CLK_REF_FREQ_HZ;
+}
+
+/*
+ * @brief  retrieves the timer 3 clock reference freq
+ *
+ * @param  None
+ * @retval freq (hz)
+ */
+uint32_t Get_TIM3CLKRefFreqHz(void) {
+    return TIM3_CLK_REF_FREQ_HZ;
+}
+
+/*
+ * @brief  start specified timer peripheral
+ *
+ * @param  htim		pointer to HAL timer handle
+ * @retval None
+ */
+void start_timer(TIM_HandleTypeDef *htim) {
 	HAL_TIM_Base_Start(htim);
 }
 
 /*
- * @brief  timer stop
+ * @brief  stop specified timer peripheral
  *
- * @param  htim		pointer to HAL timer struct
+ * @param  htim		pointer to HAL timer handle
  * @retval None
  */
-void stop_timer(TIM_HandleTypeDef *htim)
-{
+void stop_timer(TIM_HandleTypeDef *htim) {
 	HAL_TIM_Base_Stop(htim);
 }
 
 /*
- * @brief  program run time
+ * @brief  delay mcu operation for specified time period (ms)
  *
- * @param  None
- * @retval 			program runtime in ms
+ * @param  ms       delay in ms
+ * @retval None
  */
-__weak uint32_t millis(void) //
-{
-	return HAL_GetTick();
+void delay(uint32_t ms) {
+  HAL_Delay(ms);
 }
 
 /*
- * @brief  program run time
+ * @brief  provides a tick value from system clock
  *
  * @param  None
- * @retval 			program runtime in us
+ * @retval tick value (ms)
  */
-uint32_t get_timestamp(void)
-{
-	static uint32_t timestamp;
-	uint16_t dt, count, ref_clk_mhz;
-
-	/* Determine Reference Clock Freq */
-	ref_clk_mhz = APB1_CLK_FREQ_MHZ/(htim6.Init.Prescaler + 1);
-
-	/* Get Counter Value */
-	count = __HAL_TIM_GET_COUNTER(&htim6);
-
-	/* Determine Timestep Since Last Call */
-	dt = count / (ref_clk_mhz);
-
-	/* Add Timestep to Running Total */
-	timestamp += dt;
-
-	/* Reset Counter */
-	__HAL_TIM_SET_COUNTER(&htim6, 0);
-
-	return timestamp;
+uint32_t millis(void) {
+	return HAL_GetTick();
 }
--- Core/Src/common/time.h
+++ Core/Src/common/time.h
@@ -7,34 +7,19 @@
 
 #pragma once
 
-//#ifndef SRC_TIMER_H_
-//#define SRC_TIMER_H_
-//#endif /* SRC_TIMER_H_ */
-
-#include "main.h"
-
-/* Private defines -----------------------------------------------------------*/
-#define APB1_CLK_FREQ_MHZ	84
-
-/* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim6;
-
-/* Exported types ------------------------------------------------------------*/
-typedef struct timer {
-	uint8_t first_iteration;
-	uint16_t t1;
-	uint16_t t2;
-	uint16_t dt;
-	uint32_t timestamp;
-} timer;
+#include "stm32f4xx_hal.h"
 
 /* Exported functions prototypes ---------------------------------------------*/
-void delay(uint32_t ms);
-
-uint32_t get_timestamp(void);
+void Cache_TIMCLKRefFreq(TIM_HandleTypeDef *htim);
 
 void start_timer(TIM_HandleTypeDef *htim);
 
 void stop_timer(TIM_HandleTypeDef *htim);
 
-uint32_t millis(void); //__weak
+void delay(uint32_t ms);
+
+uint32_t millis(void);
+
+uint32_t Get_TIM2CLKRefFreqHz(void);
+
+uint32_t Get_TIM3CLKRefFreqHz(void);
