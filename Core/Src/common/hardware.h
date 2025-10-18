12c43aa1a71c7a181b9c4973ab8bb907e913b4a8
croman <charlesroman@icloud.com>
2025-10-17 19:17:25 -0700

    Introduce hardware header file in common/ directory
    - adds header file with externally declared HAL hardware timer handles

---
+++ Core/Src/common/hardware.h
@@ -0,0 +1,19 @@
+/*
+ * hardware.h
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: charlieroman
+ */
+
+#pragma once
+
+#include "stm32f4xx_hal.h"
+
+/*
+ * @brief hardware timer handles
+ */
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern TIM_HandleTypeDef htim6;
+extern TIM_HandleTypeDef htim8;
