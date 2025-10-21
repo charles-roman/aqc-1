9e2f3a7ae8761f4e8c84ef942a19a4158be11348
croman <charlesroman@icloud.com>
2025-10-21 16:33:35 -0700

    Update common hardware header
    - adds exported enum used to differentiate between configured and
      unconfigured peripherals
    - adds exported macros which clarify which timer handles are configured
    - cleans up comments

--- Core/Inc/common/hardware.h
+++ Core/Inc/common/hardware.h
@@ -9,11 +9,23 @@
 
 #include "stm32f4xx_hal.h"
 
-/*
- * @brief hardware timer handles
- */
+/* External variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
 extern TIM_HandleTypeDef htim6;
 extern TIM_HandleTypeDef htim8;
+
+/* Exported types ------------------------------------------------------------*/
+typedef enum {
+	UNCONFIGURED = 0U,
+	CONFIGURED   = !UNCONFIGURED
+} hw_config_t;
+
+/* Exported macros -----------------------------------------------------------*/
+#define HTIM2			CONFIGURED
+#define HTIM3			CONFIGURED
+#define HTIM4			CONFIGURED
+#define HTIM6			CONFIGURED
+#define HTIM8			CONFIGURED
+
