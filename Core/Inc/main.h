39c18ff99c96cd9270d8f3a29a7420afcea50b75
croman <charlesroman@icloud.com>
2025-10-21 17:19:31 -0700

    Commit additional modifications from mcu config update
    - updates newly configured GPIO pin macros

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -61,8 +61,10 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define LED_Pin GPIO_PIN_1
 #define LED_GPIO_Port GPIOC
-#define ARM_Pin GPIO_PIN_14
-#define ARM_GPIO_Port GPIOB
+#define ARM_Pin GPIO_PIN_2
+#define ARM_GPIO_Port GPIOC
+#define MODE_Pin GPIO_PIN_3
+#define MODE_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
