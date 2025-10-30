e80fcff26442427cb4d19dd4ea2d0b2be43b95c5
croman <charlesroman@icloud.com>
2025-10-29 23:02:28 -0700

    Update failsafe module
    - relocates right_side_up function to attitude module
    - relocates attitude_within_boundaries function to attitude module

--- Core/Inc/flight/failsafe.h
+++ Core/Inc/flight/failsafe.h
@@ -5,18 +5,6 @@
  *      Author: charlieroman
  */
 
-#pragma once
-
-//#ifndef SRC_FAILSAFE_H_
-//#define SRC_FAILSAFE_H_
-//#endif /* SRC_FAILSAFE_H_ */
-
-#include <stdint.h>
-#include "system.h"
-#include "sensors/imu/imu.h"
-
-uint8_t right_side_up(device *imu);
-
-uint8_t attitude_within_boundaries(systemState *st);
-
-//void fail_safe_mode(systemState *st);
+/*
+ * TO DO: Implement failsafe functionality
+ */
--- Core/Src/flight/failsafe.c
+++ Core/Src/flight/failsafe.c
@@ -5,42 +5,6 @@
  *      Author: charlieroman
  */
 
-#include <math.h>
-#include "flight/failsafe.h"
-#include "flight/system.h"
-#include "rx/rx.h"
-#include "sensors/imu/imu.h"
-
-/**
-  * @brief determines if quadcopter is right side up
-  *
-  * @param imu		pointer to (imu) device struct
-  * @retval ret		logical value (1 or 0)
-  */
-uint8_t right_side_up(device *imu)
-{
-	uint8_t ret;
-
-	/* Check sign of accel vector z component */
-	ret = (imu->accel.z > 0);
-
-	return ret;
-}
-
-/**
-  * @brief determines if quadcopter attitude is within limits
-  *
-  * @param st		pointer to systemState struct
-  * @retval ret		logical value (1 or 0)
-  */
-uint8_t attitude_within_boundaries(systemState *st)
-{
-	uint8_t ret;
-
-	/* Check roll & pitch angles are within tolerance */
-	ret = (fabs(st->roll.estimate) < ROLL_MAX) && (fabs(st->pitch.estimate) < PITCH_MAX);
-
-	return ret;
-}
-
-//void fail_safe_mode(systemState *st)
+/*
+ * TO DO: Implement failsafe functionality
+ */
