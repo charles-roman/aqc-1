dbe12cdae4a9f5de0e786b526a37329530887875
croman <charlesroman@icloud.com>
2025-11-01 15:18:34 -0700

    Update attitude pid controller settings in config settings header
    - breaks out pid control settings into individual macros for each
      separate attitude controller
    - adds new setting for integral command limit for each controller

--- Core/Inc/common/settings.h
+++ Core/Inc/common/settings.h
@@ -34,68 +34,95 @@
 #define CONFIG_THROTTLE_IDLE_TOLERANCE_PCT		2.0f
 
 // ATTITUDE-------------------------------------------------------------------
-#define COMP_FILT_ID							0U
-#define CONFIG_ATTITUDE_FILT					COMP_FILT_ID
+#define COMP_FILT_ID								0U
+#define CONFIG_ATTITUDE_FILT						COMP_FILT_ID
 
-#define CONFIG_COMP_FILT_GAIN_XL	 			0.02f
-#define CONFIG_COMP_FILT_GAIN_GYRO	 			(1 - CONFIG_COMP_FILT_GAIN_XL)
+#define CONFIG_COMP_FILT_GAIN_XL	 				0.02f
+#define CONFIG_COMP_FILT_GAIN_GYRO	 				(1 - CONFIG_COMP_FILT_GAIN_XL)
 
-#define CONFIG_ROLL_TAKEOFF_LIMIT_DEG			10.0f
-#define CONFIG_PITCH_TAKEOFF_LIMIT_DEG			10.0f
+#define CONFIG_ROLL_TAKEOFF_LIMIT_DEG				10.0f
+#define CONFIG_PITCH_TAKEOFF_LIMIT_DEG				10.0f
 
-// PID------------------------------------------------------------------------
-#define CONFIG_ROLL_PID_GAINS					{8.5f, 8.0f, 0.80f}
-#define CONFIG_PITCH_PID_GAINS					{11.5f, 11.0f, 0.85f}
-#define CONFIG_YAW_PID_GAINS					{7.5f, 5.5f, 0.0f}
-#define CONFIG_ROLL_CMD_LIM						150.0f
-#define CONFIG_PITCH_CMD_LIM					150.0f
-#define CONFIG_YAW_CMD_LIM						150.0f
-#define CONFIG_DERIVATIVE_LPF_CUTOFF_FREQ_RPS	2 * PI * 75.0f	// (NOTE: Might want to break this out into individual cutoff frequencies for separate controllers)
+#define CONFIG_ROLL_ANGLE_P_GAIN					8.5f
+#define CONFIG_ROLL_ANGLE_I_GAIN					8.0f
+#define CONFIG_ROLL_ANGLE_D_GAIN					0.80f
+#define CONFIG_ROLL_ANGLE_D_LPF_CUTOFF_FREQ_RPS		2 * PI * 75.0f
+#define CONFIG_ROLL_ANGLE_CMD_LIM					CONFIG_ROLL_MAX_DPS
+#define CONFIG_ROLL_ANGLE_I_CMD_LIM					CONFIG_ROLL_ANGLE_CMD_LIM * 0.3
+
+#define CONFIG_PITCH_ANGLE_P_GAIN					11.5f
+#define CONFIG_PITCH_ANGLE_I_GAIN					11.0f
+#define CONFIG_PITCH_ANGLE_D_GAIN					0.85f
+#define CONFIG_PITCH_ANGLE_D_LPF_CUTOFF_FREQ_RPS	2 * PI * 75.0f
+#define CONFIG_PITCH_ANGLE_CMD_LIM					CONFIG_PITCH_MAX_DPS
+#define CONFIG_PITCH_ANGLE_I_CMD_LIM				CONFIG_PITCH_ANGLE_CMD_LIM * 0.3
+
+#define CONFIG_ROLL_RATE_P_GAIN						7.5f
+#define CONFIG_ROLL_RATE_I_GAIN						5.5f
+#define CONFIG_ROLL_RATE_D_GAIN						0.0f
+#define CONFIG_ROLL_RATE_D_LPF_CUTOFF_FREQ_RPS		2 * PI * 75.0f
+#define CONFIG_ROLL_RATE_CMD_LIM					150.0f
+#define CONFIG_ROLL_RATE_I_CMD_LIM					CONFIG_ROLL_RATE_CMD_LIM * 0.3
+
+#define CONFIG_PITCH_RATE_P_GAIN					7.5f
+#define CONFIG_PITCH_RATE_I_GAIN					5.5f
+#define CONFIG_PITCH_RATE_D_GAIN					0.0f
+#define CONFIG_PITCH_RATE_D_LPF_CUTOFF_FREQ_RPS		2 * PI * 75.0f
+#define CONFIG_PITCH_RATE_CMD_LIM					150.0f
+#define CONFIG_PITCH_RATE_I_CMD_LIM					CONFIG_PITCH_RATE_CMD_LIM * 0.3
+
+#define CONFIG_YAW_RATE_P_GAIN						7.5f
+#define CONFIG_YAW_RATE_I_GAIN						5.5f
+#define CONFIG_YAW_RATE_D_GAIN						0.0f
+#define CONFIG_YAW_RATE_D_LPF_CUTOFF_FREQ_RPS		2 * PI * 75.0f
+#define CONFIG_YAW_RATE_CMD_LIM						150.0f
+#define CONFIG_YAW_RATE_I_CMD_LIM					CONFIG_YAW_RATE_CMD_LIM * 0.3
 
 /* SENSOR CONFIG SETTINGS----------------------------------------------------------
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -----------------------------------------------------------------------------------*/
 // IMU------------------------------------------------------------------------
-#define LSM6DSOX_DEVICE_ID						0U
-#define CONFIG_IMU_DEVICE						LSM6DSOX_DEVICE_ID
+#define LSM6DSOX_DEVICE_ID							0U
+#define CONFIG_IMU_DEVICE							LSM6DSOX_DEVICE_ID
 
-#define IMU_I2C_PROTOCOL_ID						0U
-#define CONFIG_IMU_COMM_PROTOCOL				IMU_I2C_PROTOCOL_ID
+#define IMU_I2C_PROTOCOL_ID							0U
+#define CONFIG_IMU_COMM_PROTOCOL					IMU_I2C_PROTOCOL_ID
 
-#define CONFIG_GY_LPF							DISABLED
-#define CONFIG_GY_LPF_CUTOFF_FREQ_RPS 			2 * PI * 500.0f
+#define CONFIG_GY_LPF								DISABLED
+#define CONFIG_GY_LPF_CUTOFF_FREQ_RPS 				2 * PI * 500.0f
 
-#define CONFIG_XL_LPF							DISABLED
-#define CONFIG_XL_LPF_CUTOFF_FREQ_RPS 			2 * PI * 40.0f
+#define CONFIG_XL_LPF								DISABLED
+#define CONFIG_XL_LPF_CUTOFF_FREQ_RPS 				2 * PI * 40.0f
 
 /* PROTOCOL CONFIG SETTINGS--------------------------------------------------------
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -----------------------------------------------------------------------------------*/
 // PWM------------------------------------------------------------------------
-#define CONFIG_PWM_PULSE_MIN_US 				988U
-#define CONFIG_PWM_PULSE_MAX_US 				2012U
-#define CONFIG_PWM_PULSE_PROTO_MIN_US			1000U
-#define CONFIG_PWM_PULSE_PROTO_MAX_US			2000U
-#define CONFIG_PWM_PULSE_VALID_MIN_US			950U
-#define CONFIG_PWM_PULSE_VALID_MAX_US			2050U
+#define CONFIG_PWM_PULSE_MIN_US 					988U
+#define CONFIG_PWM_PULSE_MAX_US 					2012U
+#define CONFIG_PWM_PULSE_PROTO_MIN_US				1000U
+#define CONFIG_PWM_PULSE_PROTO_MAX_US				2000U
+#define CONFIG_PWM_PULSE_VALID_MIN_US				950U
+#define CONFIG_PWM_PULSE_VALID_MAX_US				2050U
 
 /* RX CONFIG SETTINGS--------------------------------------------------------------
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -----------------------------------------------------------------------------------*/
 // PROTOCOL-------------------------------------------------------------------
-#define RX_PWM_PROTOCOL_ID						0U
-#define CONFIG_RX_PROTOCOL						RX_PWM_PROTOCOL_ID
+#define RX_PWM_PROTOCOL_ID							0U
+#define CONFIG_RX_PROTOCOL							RX_PWM_PROTOCOL_ID
 
 /* ESC CONFIG SETTINGS-------------------------------------------------------------
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -----------------------------------------------------------------------------------*/
 // PROTOCOL-------------------------------------------------------------------
-#define ESC_PWM_PROTOCOL_ID						0U
-#define CONFIG_ESC_PROTOCOL						ESC_PWM_PROTOCOL_ID
+#define ESC_PWM_PROTOCOL_ID							0U
+#define CONFIG_ESC_PROTOCOL							ESC_PWM_PROTOCOL_ID
+
 // MOTOR COMMANDS-------------------------------------------------------------
-#define CONFIG_MTR_CMD_IDLE_PCT					18.0f
-#define CONFIG_MTR_CMD_LIFTOFF_PCT				24.0f
-#define CONFIG_MTR_CMD_LIMIT_PCT				100.0f // Max is 100% (this would unlock full motor potential)
+#define CONFIG_MTR_CMD_IDLE_PCT						18.0f
+#define CONFIG_MTR_CMD_LIFTOFF_PCT					24.0f
+#define CONFIG_MTR_CMD_LIMIT_PCT					100.0f // Max is 100% (this would unlock full motor potential)
 
 /*
  * On Full Charged Battery
