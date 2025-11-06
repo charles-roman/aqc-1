8bbcfa1d950dcbacfa83ae9390e79c93abc34516
croman <charlesroman@icloud.com>
2025-11-05 17:06:58 -0800

    Update esc module interface and functionality / data ownership
    - adds esc commands struct type to mirror motor commands handle and
      handle internal management of esc commands
    - updates esc module to be the manager of esc command properties as
      opposed to individual protocol sub-modules
    - adds esc command properties struct type for internal management of and
      simpler external distribution of esc command properties
    - adds esc_get_command_properties in public interface for external code
      to access esc command properties
    - updates esc protocol interface function definitions to accept
      necessary arguments for distributing esc commands / command properties
    - renames set_motor_commands function pointer within esc protocol
      interface to set_commands
    - updates internal aliases for esc config settings to reflect latest
      naming conventions
    - renames ESC_CMD conversion function to map_pct_to_esc_cmd and updates
      command property references
    - re-locates sanitization function for esc commands to esc module as
      opposed to individual protocol sub-modules
    - updates esc command sanitization function to verify command is within
      the range from idle to limit instead of min/max and updates command
      property references
    - updates esc_set_motor_commands to handle casting and sanitization of
      requested motor commands as opposed to individual protocol sub-modules
    - removes is_armed check from esc protocol interface v-table and
      re-locates it as a standalone function within the esc module
    - updates the esc driver validation function to reflect changes to
      v-table
    - updates esc_init function to throw a compiler error on improper esc
      protocol configuration
    - updates esc_init function to validate and initialize esc command
      properties as well as esc commands itself as opposed to individual
      protocol sub-modules
    - updates esc_deinit function to reset esc commands and esc command
      properties itself as opposed to individual protocol sub-modules
    - updates calls of protocol driver functions to include relevant
      parameters as necessary

--- Core/Inc/esc/esc.h
+++ Core/Inc/esc/esc.h
@@ -7,13 +7,23 @@
 
 #pragma once
 
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
 /* Exported types ------------------------------------------------------------*/
-typedef struct mtr_cmds {
+typedef struct {
 	float mtr1;
 	float mtr2;
 	float mtr3;
 	float mtr4;
 } mtr_cmds_t;
+
+typedef struct {
+	uint32_t esc1;
+	uint32_t esc2;
+	uint32_t esc3;
+	uint32_t esc4;
+} esc_cmds_t;
 
 typedef enum {
     ESC_OK			= 0x00U,
@@ -22,15 +32,22 @@ typedef enum {
 } esc_status_t;
 
 typedef struct {
-	esc_status_t (*init)(void);
+	esc_status_t (*init)(uint32_t*, uint32_t*);
 	esc_status_t (*deinit)(void);
-	esc_status_t (*start)(void);
+	esc_status_t (*start)(uint32_t);
 	esc_status_t (*stop)(void);
-    void (*arm)(void);
-    void (*disarm)(void);
-    bool (*is_armed)(void);
-    esc_status_t (*set_motor_commands)(const mtr_cmds_t*);
+    void (*arm)(uint32_t);
+    void (*disarm)(uint32_t);
+    esc_status_t (*set_commands)(const esc_cmds_t*);
 } esc_protocol_interface_t;
+
+typedef struct {
+	uint32_t min;
+	uint32_t max;
+	uint32_t idle;
+	uint32_t limit;
+	uint32_t liftoff;
+} esc_cmd_props_t;
 
 /* Exported function prototypes ----------------------------------------------*/
 esc_status_t esc_init(void);
@@ -47,4 +64,6 @@ void esc_disarm(void);
 
 bool esc_is_armed(void);
 
-esc_status_t esc_set_motor_commands(const mtr_cmds_t *cmd);
+esc_status_t esc_set_motor_commands(const mtr_cmds_t *mcmd);
+
+void esc_get_command_properties(esc_cmd_props_t *out);
--- Core/Src/esc/esc.c
+++ Core/Src/esc/esc.c
@@ -7,23 +7,75 @@
 
 #include "esc/esc.h"
 #include "esc/protocols/pwm_esc.h"
+#include "common/maths.h"
 #include "common/settings.h"
 
 /**
-  * @brief  Esc Protocol Setting
+  * @brief  ESC Protocol Setting
   */
-#define ESC_PROTOCOL	CONFIG_ESC_PROTOCOL
+#define ESC_PROTOCOL			CONFIG_ESC_PROTOCOL
 
 /**
-  * @brief  esc driver pointer for protocol interface
+  * @brief  ESC Command Settings
+  */
+#define ESC_CMD_IDLE_PCT		CONFIG_ESC_CMD_IDLE_PCT
+#define ESC_CMD_LIFTOFF_PCT		CONFIG_ESC_CMD_LIFTOFF_PCT
+#define ESC_CMD_LIMIT_PCT		CONFIG_ESC_CMD_LIMIT_PCT
+
+/**
+  * @brief  ESC Commands Handle
+  */
+static esc_cmds_t cmd;
+
+/**
+  * @brief  ESC Command Properties Handle
+  */
+static esc_cmd_props_t cmd_props;
+
+/**
+  * @brief  ESC Driver Pointer for Protocol Interface
   */
 static const esc_protocol_interface_t *esc_driver = NULL;
+
+
+/**
+  * @brief fetches esc command properties
+  *
+  * @param	out		esc command properties handle buffer to be filled
+  * @retval None
+  */
+void esc_get_command_properties(esc_cmd_props_t *out) {
+    *out = cmd_props;
+}
+
+/**
+  * @brief calculates esc cmd based on pct
+  *
+  * @param  pct		percentage to set esc command at
+  * @retval esc command
+  */
+static inline uint32_t map_pct_to_esc_cmd(float pct) {
+	return (uint32_t) ((cmd_props.max - cmd_props.min) * (pct / 100.0f) + cmd_props.min);
+}
+
+/**
+  * @brief helper function to validate/constrain esc command
+  *
+  * @param  cmd		pointer to esc command
+  * @retval esc status
+  */
+static inline esc_status_t sanitize_esc_command(uint32_t *cmd) {
+	if (!inrange_u32(*cmd, cmd_props.idle, cmd_props.limit)) {
+		*cmd = constrain_u32(*cmd, cmd_props.idle, cmd_props.limit); // constrain within esc cmd range
+		return ESC_ERROR_WARN;
+	}
+	return ESC_OK;
+}
 
 /**
   * @brief helper function to validate esc_driver initialization
   *
   * @param  driver	pointer to esc driver
-  *
   * @retval boolean
   */
 static bool valid_esc_driver(esc_protocol_interface_t *driver) {
@@ -34,64 +86,93 @@ static bool valid_esc_driver(esc_protocol_interface_t *driver) {
 			driver->stop &&
 			driver->arm &&
 			driver->disarm &&
-			driver->is_armed &&
-			driver->set_motor_commands);
+			driver->set_commands);
 }
 
 /**
-  * @brief esc API call to init esc protocol driver interface and start comms
+  * @brief esc API call to init esc protocol driver interface
   *
-  * @param  None
   * @retval esc status
   */
 esc_status_t esc_init(void) {
 	/* Use pre-processor conditionals based on configured protocol to initialize esc_driver.
 	 * NOTE: hot-swaps would require ESC_PROTOCOL to be a modifiable variable */
 	#if ESC_PROTOCOL == ESC_PWM_PROTOCOL_ID
-	esc_driver = &pwm_esc_driver;
+		esc_driver = &pwm_esc_driver;
 	#else
-	return ESC_ERROR_FATAL;
+		#error "Invalid ESC protocol configuration"
 	#endif
 
 	if (!valid_esc_driver(esc_driver))
 		return ESC_ERROR_FATAL;
 
-	return esc_driver->init();
+	esc_status_t status = esc_driver->init(&cmd_props.min, &cmd_props.max);
+
+	/* Init remaining motor command properties */
+	cmd_props.idle = map_pct_to_esc_cmd(ESC_CMD_IDLE_PCT);
+	if (!inrange_u32(cmd_props.idle, cmd_props.min, cmd_props.max))
+		return ESC_ERROR_FATAL;
+
+	cmd_props.liftoff = map_pct_to_esc_cmd(ESC_CMD_LIFTOFF_PCT);
+	if (!inrange_u32(cmd_props.liftoff, cmd_props.min, cmd_props.max))
+		return ESC_ERROR_FATAL;
+
+	cmd_props.limit = map_pct_to_esc_cmd(ESC_CMD_LIMIT_PCT);
+	if (!inrange_u32(cmd_props.limit, cmd_props.min, cmd_props.max))
+		return ESC_ERROR_FATAL;
+
+	/* Init motor command variables */
+	cmd.esc1 = cmd_props.min;
+	cmd.esc2 = cmd_props.min;
+	cmd.esc3 = cmd_props.min;
+	cmd.esc4 = cmd_props.min;
+
+	return status;
 }
 
 /**
   * @brief esc API call to deinit esc protocol driver interface
   *
-  * @param  None
-  * @retval None
+  * @retval esc status
   */
 esc_status_t esc_deinit(void) {
 	if (!esc_driver)
 		return ESC_ERROR_WARN;
 
-	esc_driver->deinit();
+	/* Reset cached motor command properties */
+	cmd_props.min = 0U;
+	cmd_props.max = 0U;
+	cmd_props.idle = 0U;
+	cmd_props.liftoff = 0U;
+	cmd_props.limit = 0U;
+
+	/* Set motor command variables to safe state*/
+	cmd.esc1 = cmd_props.min;
+	cmd.esc2 = cmd_props.min;
+	cmd.esc3 = cmd_props.min;
+	cmd.esc4 = cmd_props.min;
+
+	esc_status_t status = esc_driver->deinit();
 	esc_driver = NULL;
 
-	return ESC_OK;
+	return status;
 }
 
 /**
   * @brief esc API call to start comms
   *
-  * @param  None
   * @retval esc status
   */
 esc_status_t esc_start(void) {
 	if (!esc_driver)
 		return ESC_ERROR_FATAL;
 
-	return esc_driver->start();
+	return esc_driver->start(cmd_props.min);
 }
 
 /**
   * @brief esc API call to stop comms
   *
-  * @param  None
   * @retval esc status
   */
 esc_status_t esc_stop(void) {
@@ -104,55 +185,78 @@ esc_status_t esc_stop(void) {
 /**
   * @brief esc API call to arm system (idles motors)
   *
-  * @param  None
   * @retval None
   */
 void esc_arm(void) {
 	if (!esc_driver)
 		return ESC_ERROR_FATAL;
 
-	return esc_driver->arm();
+	esc_driver->arm(cmd_props.idle);
 }
 
 /**
   * @brief esc API call to disarm system (stops motors)
   *
-  * @param  None
   * @retval None
   */
 void esc_disarm(void) {
 	if (!esc_driver)
 		return ESC_ERROR_FATAL;
 
-	return esc_driver->disarm();
+	esc_driver->disarm(cmd_props.min);
 }
-
 
 /**
   * @brief esc API call to check whether esc is armed
   *
-  * @param  None
-  * @retval None
+  * @retval boolean
   */
 bool esc_is_armed(void) {
-	if (!esc_driver)
-		return ESC_ERROR_FATAL;
+	if (cmd.esc1 >= cmd_props.idle)
+		return true;
 
-	return esc_driver->is_armed();
+	if (cmd.esc2 >= cmd_props.idle)
+		return true;
+
+	if (cmd.esc3 >= cmd_props.idle)
+		return true;
+
+	if (cmd.esc4 >= cmd_props.idle)
+		return true;
+
+	return false;
 }
 
 /**
   * @brief esc API call to set motor commands
   *
   * @param  cmd		pointer to motor commands handle
-  *
   * @retval esc status
   */
-esc_status_t esc_set_motor_commands(const mtr_cmds_t *cmd) {
-	/* NOTE: include this check when integrating hot-swaps or other features which may
-	 * call esc_deinit during runtime, otherwise leave lightweight due to call frequency
-	 *
-	 * if (!esc_driver) return ESC_ERROR_FATAL; */
+esc_status_t esc_set_motor_commands(const mtr_cmds_t *mcmd) {
+	esc_status_t status = ESC_OK;
 
-	return esc_driver->set_motor_commands(cmd);
+	/* Cast to Integral Type */
+	cmd.esc1 = (uint32_t) mcmd->mtr1;
+	cmd.esc2 = (uint32_t) mcmd->mtr2;
+	cmd.esc3 = (uint32_t) mcmd->mtr3;
+	cmd.esc4 = (uint32_t) mcmd->mtr4;
+
+	/* Validate Motor Commands */
+	if (sanitize_esc_command(&cmd.esc1) != ESC_OK)
+		status = ESC_ERROR_WARN;
+
+	if (sanitize_esc_command(&cmd.esc2) != ESC_OK)
+		status = ESC_ERROR_WARN;
+
+	if (sanitize_esc_command(&cmd.esc3) != ESC_OK)
+		status = ESC_ERROR_WARN;
+
+	if (sanitize_esc_command(&cmd.esc4) != ESC_OK)
+		status = ESC_ERROR_WARN;
+
+	if (!esc_driver)
+		return ESC_ERROR_FATAL;
+
+	return esc_driver->set_commands(cmd);
 }
