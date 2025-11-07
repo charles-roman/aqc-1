596383cd9384569ab6165d7b7f6665331b1f5257
croman <charlesroman@icloud.com>
2025-11-07 15:39:58 -0800

    Update common hardware header
    - removes HTIM6 from list of configured timer handle macros
    - adds macros for ports/pins of configured led, arm, mode, and sd detect
      gpios

--- Core/Inc/common/hardware.h
+++ Core/Inc/common/hardware.h
@@ -13,7 +13,6 @@
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
-extern TIM_HandleTypeDef htim6;
 extern TIM_HandleTypeDef htim8;
 
 extern I2C_HandleTypeDef hi2c1;
@@ -25,10 +24,21 @@ typedef enum {
 } hw_config_t;
 
 /* Exported macros -----------------------------------------------------------*/
-#define HTIM2			CONFIGURED
-#define HTIM3			CONFIGURED
-#define HTIM4			CONFIGURED
-#define HTIM6			CONFIGURED
-#define HTIM8			CONFIGURED
+#define HTIM2				CONFIGURED
+#define HTIM3				CONFIGURED
+#define HTIM4				CONFIGURED
+#define HTIM8				CONFIGURED
 
-#define HI2C1			CONFIGURED
+#define HI2C1				CONFIGURED
+
+#define LED_Pin 			GPIO_PIN_1
+#define LED_GPIO_Port 		GPIOC
+
+#define ARM_Pin 			GPIO_PIN_2
+#define ARM_GPIO_Port 		GPIOC
+
+#define MODE_Pin 			GPIO_PIN_3
+#define MODE_GPIO_Port 		GPIOC
+
+#define SD_DETECT_Pin 		GPIO_PIN_12
+#define SD_DETECT_GPIO_Port GPIOB
